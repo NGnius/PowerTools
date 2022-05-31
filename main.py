072053dcfca6d29e35d1eb8fd913859d3bd4690b
NGnius (Graham) <ngniusness@gmail.com>
2022-05-31 19:42:13 -0400

    Bump version to v0.7.0

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import asyncio
 import pathlib
 import subprocess
 
-VERSION = "0.6.0"
+VERSION = "0.7.0"
 HOME_DIR = str(pathlib.Path(os.getcwd()).parent.parent.resolve())
 DEFAULT_SETTINGS_LOCATION = HOME_DIR + "/.config/powertools/default_settings.json"
 LOG_LOCATION = "/tmp/powertools.log"
